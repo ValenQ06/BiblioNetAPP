1ae6bd4b0ba7be7b345ff5f1310a21abbbeb4991
ValenQ06 <vquinonesmina@gmail.com>
2022-03-17 11:25:43 -0500

    Se crea proyecto BiblioNetAPP

---
+++ BiblioNetAPP/Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+﻿using BiblioNetAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiblioNetAPP.Controllers
+{
+    public class BooksController: Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Books books)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(books);
+            }
+            return View();
+        }
+    }
+}
---
+++ BiblioNetAPP/Models/Books.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BiblioNetAPP.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+
+    }
+}
